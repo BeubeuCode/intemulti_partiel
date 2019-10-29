4226954ff5c932031fac4336ae023569ade3300f
Benoit Arnoult <benar77@gmail.com>
2019-10-29 10:05:17 +0100

    initial commit oooopsie

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+document.body.onload = bodyAppear;
+
+function bodyAppear() {
+    document.getElementById("hero").classList.remove("isgone");
+    document.getElementById("hero").classList.add("appear");
+}
+
+function showDropdown() {
+    document.getElementsByClassName("drop-content")[0].style.display = "block";
+}
+
